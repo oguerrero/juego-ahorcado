f022bd9307d493836c9bf12daa3a6a78e05c6933
oguerrero <oscarguerrero242@gmail.com>
2022-05-26 22:50:14 -0500

    Updates

---
+++ precache-manifest.a5f9d360c4d35a68298aa37048f35734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8715effc8ae979ef785",
+    "url": "/juego-ahorcado/static/css/main.04c54c85.chunk.css"
+  },
+  {
+    "revision": "a8715effc8ae979ef785",
+    "url": "/juego-ahorcado/static/js/main.ed8351d6.chunk.js"
+  },
+  {
+    "revision": "5d62cdd0f3d5a53992a5",
+    "url": "/juego-ahorcado/static/js/runtime~main.66081867.js"
+  },
+  {
+    "revision": "975cb3126350a14d1dc2",
+    "url": "/juego-ahorcado/static/css/2.1fa45c7e.chunk.css"
+  },
+  {
+    "revision": "975cb3126350a14d1dc2",
+    "url": "/juego-ahorcado/static/js/2.62ba5bbc.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/juego-ahorcado/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "bdb203f914eab8fa94f5ecd39a6a36ff",
+    "url": "/juego-ahorcado/index.html"
+  }
+];
